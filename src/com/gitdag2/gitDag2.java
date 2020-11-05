ae0e6c098ff52c566f331293dc679777fe728836
Ola Amberntsson <ola.amberntsson@iths.se>
2020-11-05 10:48:20 +0100

    Second commit

--- src/com/gitdag2/gitDag2.java
+++ src/com/gitdag2/gitDag2.java
@@ -3,6 +3,6 @@ package com.gitdag2;
 public class gitDag2 {
 
     public static void main(String[] args) {
-        System.out.println("First version");
+        System.out.println("Second version");
     }
 }
